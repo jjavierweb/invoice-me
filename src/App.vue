1e63275ea294d2b50206a78632c41ee904196b79
Javier Gutierrez <jjavierweb@outlook.com>
2022-10-22 23:52:20 -0400

    Moved route-views into the content div

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,10 @@ import Navigation from "@/components/Navigation.vue";
     <Navigation />
     <div
       class="flex flex-col sm:flex-row min-h-screen pt-0 pb-0 pl-5 pr-5 flex-1"
-    ></div>
+    >
+      <RouterView />
+    </div>
   </div>
-
-  <RouterView />
 </template>
 
 <style></style>
