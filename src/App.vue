89d7b1027b89406bce40cac7b46812287a3ef758
Javier Gutierrez <jjavierweb@outlook.com>
2022-10-22 23:50:29 -0400

    Got images in good condition and fixed styles

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,14 @@ import Navigation from "@/components/Navigation.vue";
 </script>
 
 <template>
-  <Navigation />
+  <div class="flex bg-[#141625] min-h-screen flex-col sm:flex-row">
+    <Navigation />
+    <div
+      class="flex flex-col sm:flex-row min-h-screen pt-0 pb-0 pl-5 pr-5 flex-1"
+    ></div>
+  </div>
+
   <RouterView />
 </template>
 
-<style scoped></style>
+<style></style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header class="flex">Test Me</header>
+  <header
+    class="flex z-[99] flex-row bg-[#1e2139] sm:min-h-full sm:min-w-[90px] sm:flex-col sm:rounded-tl-none sm:rounded-bl-none sm:rounded-r-[20px]"
+  >
+    <div
+      class="flex rounded-tl-none rounded-bl-none rounded-r-[20px] bg-[#7c4dfa] justify-center p-[24px] sm:w-full"
+    >
+      <img
+        src="@/assets/images/file-invoice-dollar-solid.png"
+        alt=""
+        class="w-auto h-[30px]"
+      />
+    </div>
+  </header>
 </template>
 
 <style scoped></style>
