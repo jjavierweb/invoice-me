d0c0ce6b2e3d859c00bde72049b4de893c940bfe
Javier Gutierrez <jjavierweb@outlook.com>
2022-10-23 13:21:03 -0400

    Added extra colors to tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-purple": "#252945",
+        red: "#ec5757",
+        purple: "#7c5dfa",
+        green: "#33d69f",
+        orange: "#ff8f00",
+        "very-dark-purple": "#141625",
+        "dark-gray": "#777f98",
+      },
+    },
     screens: {
       sm: "900px",
     },
