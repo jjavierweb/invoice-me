772146f8cba372281f3ba58e179d8e244670139c
Javier Gutierrez <jjavierweb@outlook.com>
2022-10-22 22:39:50 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+
+// import components
+import Navigation from "@/components/Navigation.vue";
+</script>
+
+<template>
+  <Navigation />
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="flex">Test MEs</header>
+</template>
+
+<style scoped></style>
