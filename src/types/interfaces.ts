74fb29238c4e7e7f44deaeefd4c2639701bda96a
Javier Gutierrez <jjavierweb@outlook.com>
2022-10-26 13:45:27 -0400

    Added some comments, although not that helpful

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -3,6 +3,11 @@
  * that we will use through the project
  */
 
+/**
+ * Interface for Invoice Item List
+ * Defines the object for the invoice
+ * item
+ */
 export interface InvoiceItemList {
   id: string | null;
   itemName: string | null;
