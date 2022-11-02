a068bdc4fbb3889dc227483923e97b507c62f6cc
Javier Gutierrez <jjavierweb@outlook.com>
2022-11-01 21:27:41 -0400

    Added css and the rest of the markup for the invoice details

--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -27,20 +27,274 @@ currentInvoice = currentInvoiceArray.value[0]; // cleanup for the markup
 </script>
 
 <template>
-  <div
-    v-if="currentInvoice"
-    class="container py-10 px-[10px] my-0 mx-auto sm:pt-[72px]"
-  >
+  <div v-if="currentInvoice" class="invoice-view container">
     <RouterLink class="nav-link flex" :to="{ name: 'home' }">
-      <img src="@/assets/images/icon-arrow-left.svg" alt="" /> Go Back
+      <img src="@/assets/images/icon-arrow-left.svg" alt="" />
+      Go Back
     </RouterLink>
-    <div class="flex">
+    <!-- HEADER -->
+    <div class="header flex">
       <!-- LEFT -->
-      <div class="flex"></div>
+      <div class="left flex">
+        <span>Status</span>
+        <!-- Status buttons -->
+        <div
+          class="status-button flex"
+          :class="{
+            paid: currentInvoice.invoicePaid,
+            draft: currentInvoice.invoiceDraft,
+            pending: currentInvoice.invoicePending,
+          }"
+        >
+          <span v-if="currentInvoice.invoicePaid">Paid</span>
+          <span v-if="currentInvoice.invoiceDraft">Draft</span>
+          <span v-if="currentInvoice.invoicePending">Pending</span>
+        </div>
+      </div>
       <!-- RIGHT -->
-      <div class="flex"></div>
+      <div class="right flex">
+        <button class="bg-purple p-2">Edit Invoice</button>
+        <button class="bg-red p-2 ml-2">Delete Invoice</button>
+        <button class="bg-green p-2 ml-2" v-if="currentInvoice.invoicePending">
+          Mark as Paid
+        </button>
+        <button
+          v-if="currentInvoice.invoiceDraft || currentInvoice.invoicePaid"
+          class="bg-orange p-2 ml-2"
+        >
+          Mark as Pending
+        </button>
+      </div>
+    </div>
+    <!-- Invoice Details -->
+    <div class="invoice-details flex flex-col">
+      <!-- TOP -->
+      <div class="top flex">
+        <!-- LEFT -->
+        <div class="left flex flex-col">
+          <p><span>#</span>{{ currentInvoice.invoiceId }}</p>
+          <p>{{ currentInvoice.productDescription }}</p>
+        </div>
+        <!-- RIGHT -->
+        <div class="right flex flex-col">
+          <p>{{ currentInvoice.billerStreetAddress }}</p>
+          <p>{{ currentInvoice.billerCity }}</p>
+          <p>{{ currentInvoice.billerZipCode }}</p>
+          <p>{{ currentInvoice.billerCountry }}</p>
+        </div>
+      </div>
+      <!-- MIDDLE -->
+      <div class="middle flex">
+        <!-- Payment Section -->
+        <div class="payment flex flex-col">
+          <h4>Invoice Date:</h4>
+          <p>
+            {{ currentInvoice.invoiceDate }}
+          </p>
+          <h4>Payment Date:</h4>
+          <p>
+            {{ currentInvoice.paymentDueDate }}
+          </p>
+        </div>
+        <!-- Bill To Section -->
+        <div class="bill flex flex-col">
+          <h4>Bill To:</h4>
+          <p>{{ currentInvoice.clientName }}</p>
+          <p>{{ currentInvoice.clientStreetAddress }}</p>
+          <p>{{ currentInvoice.clientCity }}</p>
+          <p>{{ currentInvoice.clientZipCode }}</p>
+          <p>{{ currentInvoice.clientCountry }}</p>
+        </div>
+        <div class="send-to flex flex-col">
+          <h4>Send to:</h4>
+          <p>{{ currentInvoice.clientEmail }}</p>
+        </div>
+      </div>
+      <!-- FOOTER -->
+      <div class="bottom flex flex-col">
+        <div class="billing-items">
+          <div class="heading flex">
+            <p>Item Name:</p>
+            <p>QTY</p>
+            <p>Price</p>
+            <p>Total</p>
+          </div>
+          <div
+            v-for="(item, index) in currentInvoice.invoiceItemList"
+            :key="index"
+            class="item flex"
+          >
+            <p>{{ item.itemName }}</p>
+            <p>{{ item.qty }}</p>
+            <p>$ {{ item.price }}</p>
+            <p>$ {{ item.total }}</p>
+          </div>
+        </div>
+        <div class="total flex">
+          <p>Amount Due:</p>
+          <p>$ {{ currentInvoice.invoiceTotal }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.invoice-view {
+  .nav-link {
+    margin-bottom: 32px;
+    align-items: center;
+    color: #fff;
+    font-size: 12px;
+    img {
+      margin-right: 16px;
+      width: 7px;
+      height: 10px;
+    }
+  }
+  .header,
+  .invoice-details {
+    background-color: #1e2139;
+    border-radius: 20px;
+  }
+  .header {
+    align-items: center;
+    padding: 24px 32px;
+    font-size: 12px;
+    .left {
+      align-items: center;
+      span {
+        color: #dfe3fa;
+        margin-right: 16px;
+      }
+    }
+    .right {
+      flex: 1;
+      justify-content: flex-end;
+      button {
+        color: #fff;
+      }
+    }
+  }
+  .invoice-details {
+    padding: 48px;
+    margin-top: 24px;
+    .top {
+      div {
+        color: #dfe3fa;
+        flex: 1;
+      }
+      .left {
+        font-size: 12px;
+        p:first-child {
+          font-size: 24px;
+          text-transform: uppercase;
+          color: #fff;
+          margin-bottom: 8px;
+        }
+        p:nth-child(2) {
+          font-size: 16px;
+        }
+        span {
+          color: #888eb0;
+        }
+      }
+      .right {
+        font-size: 12px;
+        align-items: flex-end;
+      }
+    }
+    .middle {
+      margin-top: 50px;
+      color: #dfe3fa;
+      gap: 16px;
+      h4 {
+        font-size: 12px;
+        font-weight: 400;
+        margin-bottom: 12px;
+      }
+      p {
+        font-size: 16px;
+      }
+      .bill,
+      .payment {
+        flex: 1;
+      }
+      .payment {
+        h4:nth-child(3) {
+          margin-top: 32px;
+        }
+        p {
+          font-weight: 600;
+        }
+      }
+      .bill {
+        p:nth-child(2) {
+          font-size: 16px;
+        }
+        p:nth-child(3) {
+          margin-top: auto;
+        }
+        p {
+          font-size: 12px;
+        }
+      }
+      .send-to {
+        flex: 2;
+      }
+    }
+    .bottom {
+      margin-top: 50px;
+      .billing-items {
+        padding: 32px;
+        border-radius: 20px 20px 0 0;
+        background-color: #252945;
+        .heading {
+          color: #dfe3fa;
+          font-size: 12px;
+          margin-bottom: 32px;
+          p:first-child {
+            flex: 3;
+            text-align: left;
+          }
+          p {
+            flex: 1;
+            text-align: right;
+          }
+        }
+        .item {
+          margin-bottom: 32px;
+          font-size: 13px;
+          color: #fff;
+          &:last-child {
+            margin-bottom: 0;
+          }
+          p:first-child {
+            flex: 3;
+            text-align: left;
+          }
+          p {
+            flex: 1;
+            text-align: right;
+          }
+        }
+      }
+      .total {
+        color: #fff;
+        padding: 32px;
+        background-color: rgba(12, 14, 22, 0.7);
+        align-items: center;
+        border-radius: 0 0 20px 20px;
+        p {
+          flex: 1;
+          font-size: 12px;
+        }
+        p:nth-child(2) {
+          font-size: 28px;
+          text-align: right;
+        }
+      }
+    }
+  }
+}
+</style>
