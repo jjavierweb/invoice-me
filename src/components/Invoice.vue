36565ee3a4fafc229672836c850fac0f3e6db295
Javier Gutierrez <jjavierweb@outlook.com>
2022-11-01 21:30:37 -0400

    removed the css from here and put it on app.vue

--- src/components/Invoice.vue
+++ src/components/Invoice.vue
@@ -49,39 +49,4 @@ const props = defineProps<{
 span {
   font-size: 13px;
 }
-.status-button {
-  &::before {
-    content: "";
-    width: 10px;
-    height: 10px;
-    border-radius: 50%;
-    margin-right: 8px;
-  }
-  font-size: 12px;
-  margin-right: 30px;
-  align-items: center;
-  padding: 8px 30px;
-  border-radius: 10px;
-}
-.paid {
-  &::before {
-    background-color: #33d69f;
-  }
-  color: #33d69f;
-  background-color: rgba(51, 214, 160, 0.1);
-}
-.pending {
-  &::before {
-    background-color: #ff8f00;
-  }
-  color: #ff8f00;
-  background-color: rgba(255, 145, 0, 0.1);
-}
-.draft {
-  &::before {
-    background-color: #dfe3fa;
-  }
-  color: #dfe3fa;
-  background-color: rgba(223, 227, 250, 0.1);
-}
 </style>
