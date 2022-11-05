0d6f2cf42af247626005265cb71a10ff1c5992b5
Javier Gutierrez <jjavierweb@outlook.com>
2022-11-05 15:26:40 -0400

    Added check for authentication to routes Added 404 route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import InvoiceView from "@/views/InvoiceView.vue";
+import NotFound from "@/components/NotFound.vue";
+
+// firebase
+import { auth } from "@/firebase/config";
+
+const requireAuth = (to: any, from: any, next: any) => {
+  let user = auth.currentUser;
+  if (!user) {
+    next({ name: "home" });
+  } else {
+    next();
+  }
+};
 
 const routes = [
   {
@@ -12,6 +25,13 @@ const routes = [
     path: "/invoice/:id",
     name: "invoice",
     component: InvoiceView,
+    beforeEnter: requireAuth,
+  },
+  // 404 route
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notFound",
+    component: NotFound,
   },
 ];
 
