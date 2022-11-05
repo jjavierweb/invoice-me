fefff9a21dfe7bdba391e0c4b63ccc26b179d225
Javier Gutierrez <jjavierweb@outlook.com>
2022-11-04 20:42:37 -0400

    Changed way to get the invoice list added functionality to update and delete invoice

--- src/stores/invoiceStore.ts
+++ src/stores/invoiceStore.ts
@@ -1,13 +1,16 @@
 import { defineStore } from "pinia";
 
+// import other stores
+import { useInvoiceModalStore } from "./invoiceModalStore";
+
 // import firebase getCollecion
-import getCollection from "@/composables/getCollection";
+import useCollection from "@/composables/useCollection";
 import { db } from "@/firebase/config";
-import { collection, query, onSnapshot } from "firebase/firestore";
-import { watchEffect } from "vue";
+import { collection, getDocs } from "firebase/firestore";
 
 // import interface
 import type { Invoice } from "@/types/interfaces";
+const { deleteDocument } = useCollection("invoices");
 
 // create the store
 export const useInvoiceStore = defineStore("invoiceStore", {
@@ -17,51 +20,69 @@ export const useInvoiceStore = defineStore("invoiceStore", {
     currentInvoiceArray: null as Invoice[] | null,
   }),
   actions: {
-    getInvoices() {
+    //define store
+    async getInvoices() {
       // get all data from firebase
-      const q = query(collection(db, "invoices"));
+
       // confirm there are no duplicats on current and retrived data
-      const unsub = onSnapshot(q, (snap) => {
-        snap.docs.forEach((doc) => {
-          if (!this.invoiceData.some((invoice) => invoice.id === doc.id)) {
-            const data: Invoice = {
-              id: doc.id,
-              invoiceId: doc.data().invoiceId,
-              billerStreetAddress: doc.data().billerStreetAddress,
-              billerCity: doc.data().billerCity,
-              billerZipCode: doc.data().billerZipCode,
-              billerCountry: doc.data().billerCountry,
-              clientName: doc.data().clientName,
-              clientEmail: doc.data().clientEmail,
-              clientStreetAddress: doc.data().clientStreetAddress,
-              clientCity: doc.data().clientCity,
-              clientZipCode: doc.data().clientZipCode,
-              clientCountry: doc.data().clientCountry,
-              invoiceDateUnix: doc.data().invoiceDateUnix,
-              invoiceDate: doc.data().invoiceDate,
-              paymentTerms: doc.data().paymentTerms,
-              paymentDueDateUnix: doc.data().paymentDueDateUnix,
-              paymentDueDate: doc.data().paymentDueDate,
-              productDescription: doc.data().productDescription,
-              invoiceItemList: doc.data().invoiceItemList,
-              invoiceTotal: doc.data().invoiceTotal,
-              invoicePending: doc.data().invoicePending,
-              invoiceDraft: doc.data().invoiceDraft,
-              invoicePaid: doc.data().invoicePaid,
-            };
-            this.invoiceData.push(data);
-          }
-        });
+      const snap = await getDocs(collection(db, "invoices"));
+      snap.docs.forEach((doc) => {
+        if (!this.invoiceData.some((invoice) => invoice.id === doc.id)) {
+          const data: Invoice = {
+            id: doc.id,
+            invoiceId: doc.data().invoiceId,
+            billerStreetAddress: doc.data().billerStreetAddress,
+            billerCity: doc.data().billerCity,
+            billerZipCode: doc.data().billerZipCode,
+            billerCountry: doc.data().billerCountry,
+            clientName: doc.data().clientName,
+            clientEmail: doc.data().clientEmail,
+            clientStreetAddress: doc.data().clientStreetAddress,
+            clientCity: doc.data().clientCity,
+            clientZipCode: doc.data().clientZipCode,
+            clientCountry: doc.data().clientCountry,
+            invoiceDateUnix: doc.data().invoiceDateUnix,
+            invoiceDate: doc.data().invoiceDate,
+            paymentTerms: doc.data().paymentTerms,
+            paymentDueDateUnix: doc.data().paymentDueDateUnix,
+            paymentDueDate: doc.data().paymentDueDate,
+            productDescription: doc.data().productDescription,
+            invoiceItemList: doc.data().invoiceItemList,
+            invoiceTotal: doc.data().invoiceTotal,
+            invoicePending: doc.data().invoicePending,
+            invoiceDraft: doc.data().invoiceDraft,
+            invoicePaid: doc.data().invoicePaid,
+          };
+          this.invoiceData.push(data);
+        }
         this.invoicesLoaded = true;
-      });
-      watchEffect((onInvalidate) => {
-        onInvalidate(() => unsub());
       });
     },
     setCurrentInvoice(payload: string | string[]) {
       this.currentInvoiceArray = this.invoiceData.filter((invoice: Invoice) => {
         return invoice.id === payload;
       });
+    },
+    // return array of invoices that id is not equal to requested
+    removeInvoice(payload: string | string[]) {
+      this.invoiceData = this.invoiceData.filter((invoice: Invoice) => {
+        return invoice.invoiceId !== payload;
+      });
+    },
+    // perform the update of the invoice before sending it to firebase
+    async updateInvoice(docId: string, routeId: string) {
+      this.removeInvoice(docId);
+      await this.getInvoices();
+      // toggle invoice modal
+      useInvoiceModalStore().toggleInvoice();
+      useInvoiceModalStore().toggleEditInvoice();
+      // set current invoice
+      this.setCurrentInvoice(routeId);
+    },
+    // delete item from store and firebase
+    async deleleteInvoice(docId: string, routeId: string) {
+      await deleteDocument(routeId);
+      this.removeInvoice(docId);
     },
   },
 });
