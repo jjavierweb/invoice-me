6d1d405428dd43022ffa28c69b1a63d18a55e118
Javier Gutierrez <jjavierweb@outlook.com>
2022-11-04 20:41:40 -0400

    Added update document functionality

--- src/composables/useCollection.ts
+++ src/composables/useCollection.ts
@@ -5,14 +5,21 @@ import { ref } from "vue";
 import { db } from "@/firebase/config";
 
 // import firebase required items
-import { collection, addDoc } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+import type { Invoice } from "@/types/interfaces";
 
 // create method to use
 const useCollection = (collectionName: string) => {
   const error = ref<boolean | null>(null);
   const isPending = ref<boolean | null>(null);
 
-  const addDocument = async (doc: object) => {
+  const addDocument = async (doc: Invoice) => {
     error.value = false;
     isPending.value = true;
     try {
@@ -30,7 +37,39 @@ const useCollection = (collectionName: string) => {
       error.value = err.message;
     }
   };
-  return { error, isPending, addDocument };
+
+  // update document
+  const updateCurrentDoc = async (docId: string, updatedInfo: Invoice) => {
+    isPending.value = true;
+    error.value = false;
+    const docRef = doc(db, collectionName, docId);
+    try {
+      await updateDoc(docRef, { ...updatedInfo });
+      isPending.value = false;
+      error.value = false;
+    } catch (err: any) {
+      error.value = true;
+      error.value = err.message;
+      console.log(err);
+      isPending.value = false;
+    }
+  };
+  // delete document from collection
+  const deleteDocument = async (docId: string) => {
+    // create document reference
+    const docRef = doc(db, collectionName, docId);
+    try {
+      isPending.value = true;
+      error.value = false;
+      await deleteDoc(docRef);
+    } catch (err: any) {
+      isPending.value = false;
+      error.value = err.message;
+    }
+  };
+
+  // return variables and methods
+  return { error, isPending, addDocument, updateCurrentDoc, deleteDocument };
 };
 
 export default useCollection;
