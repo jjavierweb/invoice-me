9771a0245a3aed3aabdebaa59050eebc1c6950a4
Javier Gutierrez <jjavierweb@outlook.com>
2022-11-05 15:25:46 -0400

    Added authentication check

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,20 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+// import firebase
+import { auth } from "./firebase/config";
+import { onAuthStateChanged } from "firebase/auth";
+
 import "@/assets/css/main.css";
 
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-
-app.mount("#app");
+let app: any;
+/*
+Check that firebase checks for authentication
+before we mount the app if it does not exists
+already
+*/
+onAuthStateChanged(auth, () => {
+  if (!app) {
+    app = createApp(App).use(router).use(createPinia()).mount("#app");
+  }
+});
